7b36fc6737a6463f1bb4f19d37e9740a5dfac5a3
volodyaAndryushko <volodymyr.andryushko@gmail.com>
2020-03-01 19:31:28 +0200

    initial version of project

---
+++ Trello/Trello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Trello/Trello/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trello
+{
+    class User
+    {
+        String firstName;
+        String lastName;
+        String email;
+        String password;
+
+        public User(String firstName, String lastName, String email, String password)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.password = password;
+        }
+
+        public string FirstName   // property
+        {
+            get { return firstName; }   // get method
+            set { firstName = value; }  // set method
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+    }
+}
